21f21698f08177a1854a44d07017b7b44bd158cd
kenn6711 <6658013956@student.chula.ac.th>
2025-11-14 14:37:55 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,28 +20,28 @@ else:
     # Create an OpenAI client.
     client = OpenAI(api_key=openai_api_key)
 
-    # Create a session state variable to store the chat messages. This ensures that the
+   # Create a session state variable to store the chat messages. This ensures that the
     # messages persist across reruns.
     if "messages" not in st.session_state:
         st.session_state.messages = []
-
-    # Display the existing chat messages via `st.chat_message`.
+        
+    # Display the existing chat messages via st.chat_message*.
     for message in st.session_state.messages:
-        with st.chat_message(message["role"]):
-            st.markdown(message["content"])
+        with st. chat_message(message["role"]):
+            st. markdown (message ["content"] )
 
     # Create a chat input field to allow the user to enter a message. This will display
     # automatically at the bottom of the page.
-    if prompt := st.chat_input("What is up?"):
+    if prompt := st.chat_input ("What is up?"):
 
         # Store and display the current prompt.
         st.session_state.messages.append({"role": "user", "content": prompt})
-        with st.chat_message("user"):
-            st.markdown(prompt)
-
+        with st. chat_message("user"):
+            st.markdown(prompt )
+    
         # Generate a response using the OpenAI API.
         stream = client.chat.completions.create(
-            model="gpt-3.5-turbo",
+            model="gpt-4.1",
             messages=[
                 {"role": m["role"], "content": m["content"]}
                 for m in st.session_state.messages
